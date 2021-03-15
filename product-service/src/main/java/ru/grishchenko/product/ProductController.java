4f4cd33383ff528b25480ee470f321eb595a702f
Grishenko <grishenko_sy@mail.ru>
2021-03-15 11:22:55 +0300

    fix 500 server error, add front JS

--- product-service/src/main/java/ru/grishchenko/product/ProductController.java
+++ product-service/src/main/java/ru/grishchenko/product/ProductController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/products")
+//@RequestMapping("/products")
 public class ProductController {
 
     private final ProductService productService;
--- zuul-server/src/main/java/ru/grishchenko/zuul/ZuulServerApp.java
+++ zuul-server/src/main/java/ru/grishchenko/zuul/ZuulServerApp.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +15,17 @@ public class ZuulServerApp {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulServerApp.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
     }
 }
